7d63c5d403d32ed02249fe534e3818b7f524c6d3
Russ Pitcher <russell.pitcher@canopy-cloud.com>
2017-03-16 13:45:05 +0000

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,2 +1,16 @@
-"Hello World!" | Out-File -PSPath "$($Env:PUBLIC)\Desktop\Test.txt" -Force
-"FROM THE SCRIPT"
+$DownloadObj = New-Object System.Net.Webclient
+$DestinationPath = "C:\MARSAgent.exe"
+$SourcePath = "http://aka.ms/azurebackup_agent"
+$DownloadObj.DownloadFile($SourcePath, $DestinationPath)
+
+try {
+    if (Test-Path $DestinationPath) {
+           $InstallString = "$DestinationPath /q /nu"
+           Invoke-Expression -Command $InstallString
+    } else {
+        "ERROR: Could not find $DestinationPath"
+    }
+} catch [System.Exception] {
+    "ERROR: It's all gone wrong!"
+    $_
+}
