804327999b432e8cd39e5fd5a29532f0cd88f0c2
Russ Pitcher <russell.pitcher@canopy-cloud.com>
2017-03-16 15:11:54 +0000

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,23 +1,23 @@
-Write-Output "Downloading MARSAgent.exe"
+"----- Downloading MARSAgent.exe"
 $DownloadObj = New-Object System.Net.Webclient
 $DestinationPath = "C:\MARSAgent.exe"
 $SourcePath = "http://aka.ms/azurebackup_agent"
 $DownloadObj.DownloadFile($SourcePath, $DestinationPath)
 
-Write-Verbose "Unnecessary verbose message"
+Write-Verbose "----- Unnecessary verbose message"
 try {
-    Write-Output "Checking for downloaded file"
+    "----- Checking for downloaded file"
     if (Test-Path $DestinationPath) {
-        Write-Output "File found - installing agent"
+        "----- File found - installing agent"
            $InstallString = "$DestinationPath /q /nu"
            Invoke-Expression -Command $InstallString
-        Write-Output "Installation Complete"
-        Write-Error "Unnecessary Error"
+        "----- Installation Complete"
+        Write-Error "----- Unnecessary Error"
     } else {
-        Write-Output "OUT: Could not find $DestinationPath"
+        "----- OUT: Could not find $DestinationPath"
         Write-Error "ERROR: Could not find $DestinationPath"
     }
 } catch [System.Exception] {
     Write-Error "ERROR: It's all gone wrong!"
-    Write-Output $_
+    $_
 }
